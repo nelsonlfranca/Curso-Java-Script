a68e988147002e045f675c729abb3684988233ab
nelsonlfranca <nelsonleandro8888@gmail.com>
2024-07-23 19:30:37 -0300

    Aula feita de vetores e array

--- Aula 15/ambiente.js
+++ Aula 15/ambiente.js
@@ -1,3 +1,21 @@
 let num = [5,8,2,9,3]
+num.push(1)
+// // [ 1, 2, 3, 5, 8, 9 ] Adicionou o "1"
+num.sort()
+// // [ 2, 3, 5, 8, 9 ] colocou em ordem crescente 
+console.log(num)
+// [ 5, 8, 2, 9, 3 ]
+console.log(`O vetor tem ${num[0]} posições`)
+// // O vetor tem  5 posições
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+// O primeiro valor do vetor é 5
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
+// O valor 8 está na posição 4 [ 1, 2, 3, 5, '8', 9 ]
+let pos = num.indexOf(8)
+if (pos < 0) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
 
-console.log(`Nosso vetor é o ${num}`)
---
+++ Aula 15/vetor-na-tela.js
@@ -0,0 +1,5 @@
+let num = [1,2,3,4,5,6,7,8,9]
+
+for(let pos in num){
+    console.log(`A posição ${pos} pertence ao número: ${num[pos]}`)
+}
