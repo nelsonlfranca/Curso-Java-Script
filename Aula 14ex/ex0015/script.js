8207c51ebd18726f7b0f692bb11128534217c7bb
nelsonlfranca <nelsonleandro8888@gmail.com>
2024-07-23 18:13:57 -0300

    .

--- Aula 14ex/ex0015/script.js
+++ Aula 14ex/ex0015/script.js
@@ -1,9 +1,17 @@
-var num = document.getElementById('txtnum')
-var res = document.getElementById('res')
+function contar() {
+   let num = document.getElementById('txtn')
+   let tab = document.getElementById('seltab')
+   if (num.value.length  == 0) {
+        window.alert('Por favor digite um número')
+   } else {
+    let n = Number(num.value)
+    tab.innerHTML = ""
+    for (let c = 1; c <= 10; c++) {
+     let item = document.createElement('option')
+     item.text = `${n} x ${c} = ${n*c}`
+     tab.appendChild(item)
+    } 
 
-if (num.value < 0){
-    alert('ERRO, Insira um valor válido!')
-    res.innerHTML('Impossível gerar!')
-} else{
-    alert('Tudo OK!')
+    }
+   
 }
