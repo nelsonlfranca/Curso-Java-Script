fa354f992a6ff28cd81a7b40e03e3ba517556516
nelsonlfranca <nelsonleandro8888@gmail.com>
2024-07-23 21:11:55 -0300

    Aula 16- funções

---
+++ Aula 16/função01.js
@@ -0,0 +1,13 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+console.log(parimp(133))
+
+// parimp = Chamada
+// 133 = Parâmetro que é jogado no parametro (n)
+// Par e Impar = É o retorno que é jogado no console.log
---
+++ função03.js
@@ -0,0 +1,5 @@
+// Função dentro de uma Variável
+let par = function(x) {
+    return x/2
+}
+console.log(par(10))
